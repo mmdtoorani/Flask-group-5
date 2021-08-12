1d5a5b9c5960c81355d6ffbf84a449cb543fb109
milad jarrah <milad.jarrah@gmail.com>
2021-08-13 01:03:57 +0430

    Adding Blue Print For Sign In And a Template for it

--- Maktab_Flask_project/WebBlog/blog.py
+++ Maktab_Flask_project/WebBlog/blog.py
@@ -5,8 +5,14 @@ blog_bp = Blueprint('blog', __name__)
 
 @blog_bp.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('signup.html')
+
 
 @blog_bp.route("/login/")
 def login():
     return render_template('login.html')
+
+
+@blog_bp.route("/signup/")
+def sign_up():
+    return render_template('signup.html')
