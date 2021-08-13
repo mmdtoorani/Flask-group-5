6ded36efc3468f8ea1209fc50303d81659e40933
mohammadmgh <mohammad.m.ghasemloo@gmail.com>
2021-08-13 05:37:11 +0430

    changes

--- Maktab_Flask_project/WebBlog/models.py
+++ Maktab_Flask_project/WebBlog/models.py
@@ -1,9 +1,7 @@
-from mongoengine import *
-
-connect(host="mongodb://127.0.0.1:27017/my_db")
+from .db import *
 
 
-class User(DynamicDocument):
+class User(Document):
     username = StringField(max_length=50)
     email = StringField(required=True)
     first_name = StringField(max_length=50)
--- Maktab_Flask_project/WebBlog/test2.py
+++ Maktab_Flask_project/WebBlog/test2.py
@@ -1,3 +1,55 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+
+from WebBlog.db import *
+
+blog_bp = Blueprint('blog', __name__)
+
+
+@blog_bp.route('/')
+@blog_bp.route('/home')
+def home():
+    return render_template('base.html')
+
+
+@blog_bp.route("/login/")
+def login():
+    return render_template('login.html')
+
+
+@blog_bp.route("/signup/", methods=("GET", "POST"))
+def sign_up():
+    if request.method == "POST":
+        username_form = request.form["username"]
+        first_name_form = request.form["first_name"]
+        last_name_form = request.form["first_name"]
+        email_form = request.form["first_name"]
+        phone_form = request.form["phone"]
+        password_form = request.form["password"]
+        error = None
+
+        if not username_form:
+            error = "Username is required."
+        elif not password_form:
+            error = "Password is required."
+        elif User.objects(username=username_form):
+            error = f"User {username_form} is already registered."
+
+        if error is None:
+            user_created = User(username=username_form, email=email_form, first_name=first_name_form,
+                                last_name=last_name_form,
+                                phone_number=phone_form)
+            user_created.save()
+
+            return redirect(url_for("blog.login"))
+
+        flash(error)
+
+    return render_template('signup.html')
+
+
+-------------------
+
+
 {% extends 'base.html' %}
 
 {% block title %}{% endblock %}
@@ -53,3 +105,27 @@
     </div>
 {% endblock %}
 {% block extra_footer %}{% endblock %}
+
+
+
+
+---------------------
+
+
+
+
+from mongoengine import *
+
+connect(host="mongodb://127.0.0.1:27017/my_db")
+
+
+class User(DynamicDocument):
+    username = StringField(max_length=50)
+    email = StringField(required=True)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
+    phone_number = StringField(max_length=50)
+
+
+
+-----------------------
