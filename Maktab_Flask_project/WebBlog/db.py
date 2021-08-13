c355460fc68ecbd01c4b6823679286513417ff16
mohamad hosein <mmdtrni@gmail.com>
2021-08-13 17:04:45 +0430

    testing connection to database

--- Maktab_Flask_project/WebBlog/db.py
+++ Maktab_Flask_project/WebBlog/db.py
@@ -10,3 +10,10 @@ class User(DynamicDocument):
     last_name = StringField(max_length=50)
     phone_number = StringField(max_length=50)
     password = StringField(max_length=50)
+
+
+user1 = User(username='mohamad', email='mohamad@gmail.com', first_name='mmd', last_name='toorani',
+             phone_number='0123456789', password='123456')
+
+print(type(user1))
+print(user1.id)
