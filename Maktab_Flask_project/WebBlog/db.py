8d3fa13744a07bd6fd0b192c4736240cc1fbe3e6
mohammadmgh <mohammad.m.ghasemloo@gmail.com>
2021-08-13 05:57:10 +0430

    adding three fields to user

--- Maktab_Flask_project/WebBlog/db.py
+++ Maktab_Flask_project/WebBlog/db.py
@@ -4,3 +4,6 @@ connect(host="mongodb://127.0.0.1:27017/my_db")
 
 class User(DynamicDocument):
     username = StringField(max_length=50)
+    email = StringField(required=True)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
