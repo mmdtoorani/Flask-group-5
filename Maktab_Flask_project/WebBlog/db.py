ffc904bb46b17c487bc991b8a250baa91033de39
mohammadmgh <mohammad.m.ghasemloo@gmail.com>
2021-08-16 16:42:46 +0430

    adding example for understanding db

--- Maktab_Flask_project/WebBlog/db.py
+++ Maktab_Flask_project/WebBlog/db.py
@@ -45,3 +45,27 @@ class Post(DynamicDocument):
 # with open('marmot.jpg', 'rb') as fd:
 #     marmot.photo.put(fd, content_type = 'image/jpeg')
 # marmot.save()
+
+
+
+# some examples of db
+# tag1 = Tag(name='johnny depp')
+# tag1.save()
+# tag2 = Tag(name='pirate')
+# tag2.save()
+#
+# cat1 = Category(name='film')
+# cat2 = Category(name='comedy', parent_cat=cat1)
+# cat1.save()
+# cat2.save()
+#
+# user1 = User(username='mohammad', email='mohammad@gmail.com', first_name='mohammad', lastname='mohammad',
+#              phone_number='09371418637', password='mohammad')
+# user1.save()
+#
+# post1 = Post(title='pirates', body='loskvmkffvakflv', status=3, user=user1, cat=cat2, likes=[user1])
+# post1.save()
+#
+# print(cat2.id)
+# print(post1.tags)
+# print(post1.likes)
