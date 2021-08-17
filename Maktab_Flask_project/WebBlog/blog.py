7e417499661cb21a62b69f0474c9f284a823808a
mohammadmgh78 <84505068+mohammadmgh78@users.noreply.github.com>
2021-08-17 05:47:15 +0430

    Update blog.py

--- Maktab_Flask_project/WebBlog/blog.py
+++ Maktab_Flask_project/WebBlog/blog.py
@@ -70,26 +70,4 @@ def sign_up():
     return render_template('signup.html')
 
 
-@login_required
-@blog_bp.route("/create", methods=("GET", "POST"))
-def create():
-    """Create a new post for the current user."""
-    if request.method == "POST":
-        title_form = request.form["title"]
-        body_form = request.form["body"]
-        user_id_form = session['user_id']
-        f = request.files.get('image')
-        fname = secure_filename(f.filename)
-        f.save('WebBlog/static/' + fname)
-        image = fname
-        error = None
-        if not title_form:
-            error = "Title is required."
-        if error is not None:
-            flash(error)
-        else:
-            post_created = Post(title=title_form, body=body_form, user=User.objects(id=user_id_form)[0], photo=image)
-            post_created.save()
-            return redirect(url_for("blog.home"))
 
-    return render_template("create.html")
