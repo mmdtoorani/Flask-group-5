76c9466b1f2505124913f6d82fbbbc71508c150a
mohamad hosein <mmdtrni@gmail.com>
2021-08-18 16:32:49 +0430

    move create route

--- Maktab_Flask_project/WebBlog/blog.py
+++ Maktab_Flask_project/WebBlog/blog.py
@@ -67,40 +67,18 @@ def sign_up():
             error = f"User {username_form} is already registered."
 
         if error is None:
-            user_created = User(username=username_form, email=email_form, first_name=first_name_form,
+            user_created = User(username=username_form,
+                                email=email_form,
+                                first_name=first_name_form,
                                 last_name=last_name_form,
-                                phone_number=phone_form, password=generate_password_hash(password_form))
+                                phone_number=phone_form,
+                                password=password_form)
             user_created.save()
 
             return redirect(url_for("blog.login"))
 
         flash(error)
     return render_template('signup.html')
-
-
-@login_required
-@blog_bp.route("/create", methods=("GET", "POST"))
-def create():
-    """Create a new post for the current user."""
-    if request.method == "POST":
-        title_form = request.form["title"]
-        body_form = request.form["body"]
-        user_id_form = session['user_id']
-        f = request.files.get('image')
-        fname = secure_filename(f.filename)
-        f.save('WebBlog/static/' + fname)
-        image = fname
-        error = None
-        if not title_form:
-            error = "Title is required."
-        if error is not None:
-            flash(error)
-        else:
-            post_created = Post(title=title_form, body=body_form, user=User.objects(id=user_id_form)[0], photo=image)
-            post_created.save()
-            return redirect(url_for("blog.home"))
-
-    return render_template("create.html")
 
 
 @blog_bp.route("/logout")
