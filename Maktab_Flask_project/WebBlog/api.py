c38679855a567b1aefab697bd7dccffe82b3c1c3
mohamad hosein <mmdtrni@gmail.com>
2021-08-20 04:57:21 +0430

    add list-post api

--- Maktab_Flask_project/WebBlog/api.py
+++ Maktab_Flask_project/WebBlog/api.py
@@ -1,11 +1,25 @@
-from flask import Blueprint, render_template, request, url_for, redirect
-
+from flask import Blueprint, session, render_template, request, url_for, redirect, jsonify
+from db import Post, User
+import json
+import pprint
 api_bp = Blueprint('api', __name__)
 
 
 @api_bp.route("/post_list/")
 def list_post():
-    pass
+    posts = []
+    for post in Post.objects:
+        context = {
+            '_id': str(post.id),
+            'user_id': str(post.user),
+            'title': post.title,
+            'photo': post.photo,
+            'body': post.body,
+        }
+        posts.append(context)
+
+    json_post = json.loads(json.dumps(posts))
+    return jsonify(json_post)
 
 
 @api_bp.route("/post-delete/<post_id>")
@@ -28,11 +42,11 @@ def list_tags():
     pass
 
 
-@user_bp.route("/search/")
+@api_bp.route("/search/")
 def search():
     pass
 
 
-@user_bp.route("/user-profile/<user_id>")
+@api_bp.route("/user-profile/<user_id>")
 def user_profile():
     pass
