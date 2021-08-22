6759ccb52cd28610c9b2fc5bbc39b271467ce0f3
mohamad hosein <mmdtrni@gmail.com>
2021-08-23 02:36:38 +0430

    some test for db

--- Maktab_Flask_project/WebBlog/db.py
+++ Maktab_Flask_project/WebBlog/db.py
@@ -45,7 +45,6 @@ class Post(DynamicDocument):
 # marmot.save()
 
 
-
 # some examples of db
 # tag1 = Tag(name='johnny depp')
 # tag1.save()
@@ -58,14 +57,12 @@ class Post(DynamicDocument):
 # cat2.save()
 #
 # user1 = User(username='mohammad', email='mohammad@gmail.com', first_name='mohammad', lastname='mohammad',
-#              phone_number='09371418637', password='mohammad')
+#               phone_number='09371418637', password='mohammad')
 # user1.save()
 #
-# post1 = Post(title='pirates', body='loskvmkffvakflv', status=3, user=user1, cat=cat2, likes=[user1])
+# post1 = Post(title='pirates', body='loskvmkffvakflv', status=1, user=user1)
 # post1.save()
 #
 # print(cat2.id)
 # print(post1.tags)
 # print(post1.likes)
-
-
