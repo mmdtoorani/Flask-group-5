5140328cc4c5fcac7ee40c90cd9750a8f5eb7c6a
mohamad hosein <mmdtrni@gmail.com>
2021-08-27 15:30:50 +0430

    comments

--- Maktab_Flask_project/WebBlog/static/js/tag_cat.js
+++ Maktab_Flask_project/WebBlog/static/js/tag_cat.js
@@ -3,11 +3,11 @@ $.ajax({
     type: "GET",
 
     success: function (response) {
-        console.log(response);
+        // console.log(response);
 
         for (let i = 0; i < response.length; i++) {
             let login = 'login';
-            console.log(response[i])
+            // console.log(response[i])
             let tr_elem = $('<tr></tr>').appendTo('#table_tag')
             tr_elem.append(`<td onclick="redirectTo('${response[i].id}');" style="color: red" style="color: red">${response[i].name}</td>`);
         }
@@ -20,14 +20,14 @@ $.ajax({
     type: "GET",
 
     success: function (response) {
-        console.log(response);
+        // console.log(response);
 
         for (let i = 0; i < response.length; i++) {
-            console.log(response[i])
+            // console.log(response[i])
             let tr_elem = $('<tr></tr>').appendTo('#table_cat')
             tr_elem.append(`<td onclick="redirectToCat('${response[i].id}');">${response[i].name}</td>`);
             for (let j = 0; j < response[i].children.length; j++) {
-                console.log(response[i].children[j])
+                // console.log(response[i].children[j])
                 let tr_elem = $('<tr></tr>').appendTo('#table_cat')
                 tr_elem.append(`<td onclick="redirectToCat('${response[i].children[j].id}');" style="color: red">${response[i].children[j].name}</td>`);
             }
